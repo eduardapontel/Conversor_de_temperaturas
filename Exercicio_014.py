55933144dda5a644b2c8bad22140d538b27d0120
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2022-09-08 09:03:42 -0300

    Add files via upload

---
+++ Exercicio_014.py
@@ -0,0 +1,24 @@
+bordas = '\033[037m-=-\033[m' * 12
+print(bordas)
+print('{:^35}'.format('CONVERSOR DE TEMPERATURAS'))
+print(bordas)
+print()
+
+c = float(input('Informe a temperatura em °C: '))
+
+print(bordas)
+print('F -> Para converter para Fahrenheit')
+print('K -> Para converter para Kelvin')
+print(bordas)
+
+while True:
+    convert = input('Digite a opção desejada: ').strip().upper()
+    match convert:
+        case 'F':
+            print(f'\nA temperatura corresponde a {c/5*9+32:.2f}°F')
+            break
+        case 'K':
+            print(f'\nA temperatura corresponde a {c+273.15:.2f}K')
+            break
+        case _:
+            print('Por favor digite uma opção válida!')
